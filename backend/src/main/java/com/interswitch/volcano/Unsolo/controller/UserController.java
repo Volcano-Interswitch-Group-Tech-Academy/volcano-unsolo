48c6695314436474c74f13fab9ed48b813c71206
tuyojr <olutuyod@gmail.com>
2023-09-04 14:12:08 +0100

    resolved merge conflicts

--- backend/src/main/java/com/interswitch/volcano/Unsolo/controller/UserController.java
+++ backend/src/main/java/com/interswitch/volcano/Unsolo/controller/UserController.java
@@ -1,18 +1,13 @@
 package com.interswitch.volcano.Unsolo.controller;
 
-
-<<<<<<< HEAD
 import lombok.RequiredArgsConstructor;
-=======
 import com.interswitch.volcano.Unsolo.dtos.SignUpRequestDto;
 import com.interswitch.volcano.Unsolo.services.UserService;
 import jakarta.validation.Valid;
-import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
->>>>>>> 09962f6096cd5af2aea02cf4da75b722f29d775e
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -20,9 +15,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RequiredArgsConstructor
 @RequestMapping("/api/users")
 public class UserController {
-<<<<<<< HEAD
-
-=======
     private final UserService userService;
 
     @PostMapping("/sign-up")
@@ -30,5 +22,4 @@ public class UserController {
         userService.registerUser(signUpRequestDto);
         return new ResponseEntity<>("Registration Successful! Check your mail for activation link", HttpStatus.CREATED);
     }
->>>>>>> 09962f6096cd5af2aea02cf4da75b722f29d775e
 }
--- backend/src/main/java/com/interswitch/volcano/Unsolo/services/UserService.java
+++ backend/src/main/java/com/interswitch/volcano/Unsolo/services/UserService.java
@@ -1,13 +1,8 @@
 package com.interswitch.volcano.Unsolo.services;
 
-<<<<<<< HEAD
-public interface UserService {
-=======
+
 import com.interswitch.volcano.Unsolo.dtos.SignUpRequestDto;
 import com.interswitch.volcano.Unsolo.dtos.SignUpResponseDto;
-
 public interface UserService {
     SignUpResponseDto registerUser(SignUpRequestDto signUpRequestDto);
-
->>>>>>> 09962f6096cd5af2aea02cf4da75b722f29d775e
 }
