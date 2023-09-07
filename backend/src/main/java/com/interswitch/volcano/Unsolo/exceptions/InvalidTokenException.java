c7e76fd7f6c19b5704111d70e877e54fb3640fa3
Olaitan Olayinka <olayinka.olaitan@interswitchgroup.com>
2023-09-07 18:29:49 +0100

    mail configurations

---
+++ backend/src/main/java/com/interswitch/volcano/Unsolo/exceptions/InvalidTokenException.java
@@ -0,0 +1,9 @@
+package com.interswitch.volcano.Unsolo.exceptions;
+
+public class InvalidTokenException extends RuntimeException {
+
+    public InvalidTokenException(String message) {
+
+        super(message);
+    }
+}
