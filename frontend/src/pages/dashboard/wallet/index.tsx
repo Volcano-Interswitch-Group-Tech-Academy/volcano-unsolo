fbb352a1cb5cb67e37222dcd764e9d961410382c
Arinze Ezeokwuegbu <darlingtonarinze1995@gmail.com>
2023-09-21 11:06:34 +0100

    wallet page on progress

--- frontend/src/pages/dashboard/wallet/index.tsx
+++ frontend/src/pages/dashboard/wallet/index.tsx
@@ -1,10 +1,11 @@
+import Card from '@/components/card/Card'
 import DashboardLayout from '@/components/common/layout/DashboardLayout'
 import React from 'react'
 
 const wallet = () => {
   return (
     <DashboardLayout>
-      
+      <Card/>
     </DashboardLayout>
   )
 }
